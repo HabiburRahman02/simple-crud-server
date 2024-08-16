1402a4e2d9bad0aa2804e377a002cf7af9ef0194
Habibur Rahman <habibur88454@gmail.com>
2024-08-16 17:53:39 +0600

    create a update form and update using  patch

--- index.js
+++ index.js
@@ -33,6 +33,29 @@ async function run() {
       res.send(result);
     });
 
+    app.get('/users/:id', async (req, res) => {
+      const id = req.params.id
+      const query = { _id: new ObjectId(id) }
+      const result = await usersCollection.findOne(query);
+      res.send(result)
+    });
+
+    app.patch('/users/:id', async (req, res) => {
+      const id = req.params.id;
+      const updatedUser = req.body;
+      console.log(updatedUser)
+      const filter = { _id: new ObjectId(id) };
+      const updateDoc = {
+        $set: {
+          name: updatedUser.name,
+          email: updatedUser.email,
+        },
+      };
+      const result = await usersCollection.updateOne(filter, updateDoc);
+      res.send(result)
+
+    })
+
     app.post("/users", async (req, res) => {
       const user = req.body;
       const result = await usersCollection.insertOne(user);
@@ -40,8 +63,8 @@ async function run() {
     });
 
     app.delete('/users/:id', async (req, res) => {
-      const id =req.params.id;
-      const query = {_id: new ObjectId(id)};
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
       const result = await usersCollection.deleteOne(query);
       res.send(result)
       console.log(result)
